2854971a7d2644e8476f71c6edec376cf9c8c642
Josh Birch <joshuabirch@protonmail.com>
2025-04-17 15:19:49 +0100

    feat: add method to populate minefield; refactor: unify brace style
    - Added method to populate the minefield (not used in main code yet, but passes test)
    - Refactored brace style to same-line format across code (adopting this convention going forward)

--- src/Minesweeper.java
+++ src/Minesweeper.java
@@ -2,9 +2,10 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.Random;
 
-public class Minesweeper
-{
+public class Minesweeper {
     private JPanel rootPanel;
     private JButton restartButton;
     private JLabel minesRemaining;
@@ -14,13 +15,11 @@ public class Minesweeper
     private final int height = 5;
     private ActionListener mineButtonListener;
 
-    public static void main(String[] args)
-    {
+    public static void main(String[] args) {
         Minesweeper minesweeper = new Minesweeper();
     }
 
-    public Minesweeper()
-    {
+    public Minesweeper() {
         JFrame frame = new JFrame("Minesweeper!");
         frame.setContentPane(rootPanel);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -28,16 +27,14 @@ public class Minesweeper
         frame.setVisible(true);
     }
 
-    private void createUIComponents()
-    {
+    private void createUIComponents() {
         // TODO: place custom component creation code here
         GridLayout mineLayout = new GridLayout(height, width);
         minePanel = new JPanel();
         minePanel.setLayout(mineLayout);
         setupMineButtonListener();    // mine button listener must be created before the buttons
 
-        for (int i = 0; i < (width * height); i++)
-        {
+        for (int i = 0; i < (width * height); i++) {
             JButton mineButton = new JButton(" ");
             mineButton.addActionListener(mineButtonListener);
             mineButton.setActionCommand(String.valueOf(i));
@@ -46,17 +43,110 @@ public class Minesweeper
     }
 
     // instantiate listener
-    void setupMineButtonListener()
-    {
-        mineButtonListener = new ActionListener()
-        {
+    void setupMineButtonListener() {
+        mineButtonListener = new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e)
-            {
+            public void actionPerformed(ActionEvent e) {
                 System.out.println("Button " + e.getActionCommand() + " pressed");
                 // TODO: determine position of mine in grid and handle
-
             }
         };
+    }
+
+    class Coordinate {
+        private int x, y;
+
+        Coordinate(int x, int y) {
+            this.x = x;
+            this.y = y;
+        }
+
+        int getX() {
+            return x;
+        }
+
+        int getY() {
+            return y;
+        }
+
+        @Override
+        public boolean equals(Object obj) {
+            if (obj instanceof Point) {
+                Point otherPoint = (Point) obj;
+                return this.x == otherPoint.x && this.y == otherPoint.y;
+            }
+            return false;
+        }
+    }
+
+    public class MineInfo {
+        private boolean isMine = false;
+        private int adjacentMineCount = 0;
+
+        public boolean isMine() {
+            return isMine;
+        }
+
+        public void setMine(boolean mine) {
+            isMine = mine;
+        }
+
+        public int getAdjacentMineCount() {
+            return adjacentMineCount;
+        }
+
+        public void setAdjacentMineCount(int adjacentMineCount) {
+            this.adjacentMineCount = adjacentMineCount;
+        }
+    }
+
+    public class MineField {
+
+        private boolean started = false;
+        private int mineCount;
+        private int width, height;
+        private MineInfo[][] field;
+
+        MineField(int width, int height) {
+            field = new MineInfo[height][width];
+            this.width = width;
+            this.height = height;
+
+            mineCount = (width * height) / 5;
+
+            for (int y = 0; y < height; y++) {
+                for (int x = 0; x < width; x++) {
+                    field[y][x] = new MineInfo();
+                }
+            }
+        }
+
+        public int getMineCount() {
+            return mineCount;
+        }
+
+        private Coordinate getRandomCoordinate() {
+            Random rand = new Random();
+            int x = rand.nextInt(width);
+            int y = rand.nextInt(height);
+            return new Coordinate(x, y);
+        }
+
+        // populates mines avoiding player start location
+        private void populateMines(int count, Coordinate startPoint) {
+            ArrayList<Coordinate> mineLocations = new ArrayList<>();
+
+            do {
+                final Coordinate randomCoordinate = getRandomCoordinate();
+                if (!startPoint.equals(randomCoordinate) && !mineLocations.contains(randomCoordinate)) {
+                    mineLocations.add(randomCoordinate);
+                }
+            } while (mineLocations.size() < count);
+            for (Coordinate mineLocation : mineLocations) {
+                field[mineLocation.getY()][mineLocation.getX()].setMine(true);
+            }
+        }
+
+        // check each of any given cells neighbours to private count of adjacent mines
     }
 }
