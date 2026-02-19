fa8d66b892c123cb2f82256a417e4e5ebebc83b3
Josh Birch <joshuabirch@protonmail.com>
2026-02-19 18:11:51 +0000

    Style map to fit theme (b&w) and some other styling adjustments

--- scripts.js
+++ scripts.js
@@ -2,16 +2,16 @@
   - Adding a map using JS: https://leafletjs.com/
 */
 
-var map = L.map('map').setView([53.1927993, -2.889644], 13);
+var map = L.map('map').setView([53.1927993, -2.889644], 15);
 
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
 
-var circle = L.circle([53.1927993, -2.889644], {
+var circle = L.circle([53.1917993, -2.889644], {
     color: 'red',
-    fillColor: '#f03',
+    fillColor: 'rgb(201, 48, 48)',
     fillOpacity: 0.5,
-    radius: 500
+    radius: 200
 }).addTo(map);
 
