629601b5c2bdd406e876177722a41a4aea5e370a
Josh Birch <joshuabirch@protonmail.com>
2026-03-07 14:11:11 +0000

    Move frontend files into correct folder, add Favicon to live site

--- AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
+++ AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a81d9c32dba74954a32befddc15e8e94f952b1fc")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6ff27aa98aaa3d2f732b784864642f4ff8ed2b9")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
