58f211563f22c8f7bc70ed12e05098d90a82d6b0
Josh Birch <joshuabirch@protonmail.com>
2026-03-11 12:50:38 +0000

    Update _Layout.cshtml to follow front end styling and nav ready to implement across pages

--- AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
+++ AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+64a28999c055067a400bffd3fc38357cfc349a71")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+42b2580bdb94b6cf70183c690eeeeaba9cda58f8")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
