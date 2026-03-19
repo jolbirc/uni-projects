4941d87acff157b09de53641ce093679a9d7622e
Josh Birch <joshuabirch@protonmail.com>
2026-03-19 10:15:31 +0000

    Add auth to db

--- AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
+++ AirbnbGuesthouseSite/obj/Debug/net10.0/AirbnbGuesthouseSite.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+763242115d92092af68d86d5adeebcb29528986e")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1d89333c0bfae67764d55a9515ff5b475badacb2")]
 [assembly: System.Reflection.AssemblyProductAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyTitleAttribute("AirbnbGuesthouseSite")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
