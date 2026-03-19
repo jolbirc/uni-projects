feac89a8e332bf566c8da7a748fca390408fc043
Josh Birch <joshuabirch@protonmail.com>
2026-03-19 17:51:41 +0000

    Make rooms list dynamic as per databse entries

--- AirbnbGuesthouseSite/Pages/Details.cshtml.cs
+++ AirbnbGuesthouseSite/Pages/Details.cshtml.cs
@@ -1,11 +1,23 @@
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using AirbnbGuesthouseSite.Data;
+using AirbnbGuesthouseSite.Models;
 
 namespace AirbnbGuesthouseSite.Pages;
 
 public class Details : PageModel
 {
-    public void OnGet()
+    private readonly AirbnbGuesthouseSiteContext _context;
+
+    public Details(AirbnbGuesthouseSiteContext context)
     {
-        
+        _context = context;
+    }
+
+    public IList<Models.Room> Rooms { get; set; } = new List<Models.Room>();
+
+    public async Task OnGetAsync()
+    {
+        Rooms = await _context.Rooms.ToListAsync();
     }
 }
