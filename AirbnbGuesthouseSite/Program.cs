72b4aebe58fd14342e656ca36a3a8f57f7fcb4fd
Josh Birch <joshuabirch@protonmail.com>
2026-03-19 11:35:23 +0000

    Add admin login, admin tab on global nav, style admin login page

--- AirbnbGuesthouseSite/Program.cs
+++ AirbnbGuesthouseSite/Program.cs
@@ -5,20 +5,25 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using AirbnbGuesthouseSite.Data;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
+using AirbnbGuesthouseSite.Areas.Identity.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddDefaultIdentity<IdentityUser>(options  => options.SignIn.RequireConfirmedAccount = false).AddEntityFrameworkStores<AirbnbGuesthouseSiteContext>();
-builder.Services.AddRazorPages();
+builder.Services.AddRazorPages(options =>
+{
+    options.Conventions.AuthorizeFolder("/Room");
+});
 builder.Services.AddDbContext<AirbnbGuesthouseSiteContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("AirbnbGuesthouseSiteContext") ?? throw new InvalidOperationException("Connection string 'AirbnbGuesthouseSiteContext' not found.")));
 builder.Services.AddDatabaseDeveloperPageExceptionFilter();
 builder.Services.ConfigureApplicationCookie(options =>
 {
-    options.LoginPath = "Identity/Account/Login";
-    options.AccessDeniedPath = "Identity/Account/AccessDenied";
+    options.LoginPath = "/Identity/Account/Login";
+    options.AccessDeniedPath = "/Identity/Account/AccessDenied";
 });
 
 var app = builder.Build();
